f81abbb8cb96855a6a4e582dbf4cafcbaf4f1882
konazin <m4caun4@gmail.com>
2026-05-03 16:36:54 -0300

    second commit, some progression

---
+++ modules/zipCrack.c
@@ -0,0 +1,11 @@
+#include "passGen.h"
+#include "zipCrack.h"
+#include <zip.h>
+
+void ZipCrack(int lenght){
+    int control = 1;
+    while(control == 1){
+        
+    }
+    
+}
---
+++ modules/zipCrack.h
@@ -0,0 +1,6 @@
+#ifndef ZIP_CRACK_H
+#define ZIP_CRACK_H
+
+void ZipCrack(int lenght);
+
+#endif
