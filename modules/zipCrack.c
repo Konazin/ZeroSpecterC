56a92e9b83857c823a991ddc71165e06912575e2
konazin <m4caun4@gmail.com>
2026-05-04 07:36:34 -0300

    near finished

--- modules/zipCrack.c
+++ modules/zipCrack.c
@@ -1,11 +1,13 @@
 #include "passGen.h"
 #include "zipCrack.h"
+#include <string.h>
 #include <zip.h>
 
-void ZipCrack(int lenght){
+void ZipCrack(int lenght, char path){
     int control = 1;
     while(control == 1){
-        
+        char senha[6767];
+        zip_t *za = zip_fopen_encrypted(za, path, 0, senha);
     }
     
 }
